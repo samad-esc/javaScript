f32c31ae952e834ba2f17fdf505242c8bc3c21f9
Samad Mirza <abdul_ug_24@ece.nits.ac.in>
2025-10-17 18:15:44 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3+3 ) // we are using node js and not browser
+
+console.log(3 + 3);
+console.log("hitesh");
+
+let name = "samad"
+let age = 18
+let islogin = false
+
+// number 2^53
+// big int
+// string => ""
+// boolean => true false
+// NULL => stand alone value 
+// undefined => value assign nahi hui hai
+// symbol => uniqueness
+
+
+// objects
+
+console.log(typeof null); //object
+
