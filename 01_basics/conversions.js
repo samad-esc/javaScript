8a1097a89ef77248438ec049479101edc97ce704
Samad Mirza <abdul_ug_24@ece.nits.ac.in>
2025-10-17 18:29:32 +0000

    conversion of data types

---
+++ 01_basics/conversions.js
@@ -0,0 +1,28 @@
+// let score =null
+
+// // const {score} = req.body
+// console.log(typeof score);
+
+// let value=Number(score)
+// console.log(typeof value);
+// console.log(value);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 , false => 0
+
+// let istrue = "";
+// let booleanis = Boolean(istrue);
+// console.log(booleanis);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "samad"=>true
+
+
+let koinumber = 33
+let stringkanumber = String(koinumber)
+console.log(stringkanumber);
+console.log(typeof stringkanumber);
+
