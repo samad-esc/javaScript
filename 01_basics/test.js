dbe67a0b80ba32b230e528ca588dc5701279f12a
Samad Mirza <abdul_ug_24@ece.nits.ac.in>
2025-10-17 17:59:37 +0000

    container config done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,4 @@
 console.log("samad")
+//variables and datatypes
+
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,19 @@
+const accountId = 2414085 //change nahi kar sakte
+let accountEmail = "samad@google.com"
+var accountPassword = "peddu"
+accountCtiy = "Jaipur"
+let accountState
+
+// let and var are used to declare variables 
+
+// java script ko pata nahi tha ki scopes kya hote hai isliye  var nahi use karte hai ab
+// prefer not to use var because it has issues with block scope 
+// accountId = 2 not allowed
+
+console.log(accountId);
+
+accountEmail = "mirza@google.com" 
+accountPassword = "peddu1234"
+accountCtiy = "goa"
+
+console.table([accountId,accountEmail,accountPassword,accountCtiy,accountState])
