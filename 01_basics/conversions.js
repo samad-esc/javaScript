7441c4016558b6d9eed2e0ed61691fa74ab8e13a
Samad Mirza <abdul_ug_24@ece.nits.ac.in>
2025-10-22 14:48:44 +0000

    config

--- 01_basics/conversions.js
+++ 01_basics/conversions.js
@@ -21,8 +21,37 @@
 // "samad"=>true
 
 
-let koinumber = 33
-let stringkanumber = String(koinumber)
-console.log(stringkanumber);
-console.log(typeof stringkanumber);
+// let koinumber = 33
+// let stringkanumber = String(koinumber)
+// console.log(stringkanumber);
+// console.log(typeof stringkanumber);
+
+
+// *********************oprrations*******************
+
+// let str1 = "hello"
+// let str2 = " how are you"
+// let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log("1"+"2");
+// console.log(2 + 2 + "1" + 2 + 2);
+// console.log( 3*4%5%6/4);
+
+// console.log(-true);
+// console.log(+"");
+
+
+// let num1 ,num2, num3
+// num1 = num2 = num3 = 2+2
+
+
+// let gamectr = 1;
+// gamectr++
+// console.log(gamectr);
+
+
+
+
 
