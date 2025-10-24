ed615f933d87acad15d28a2a69028d5c15935046
Samad Mirza <abdul_ug_24@ece.nits.ac.in>
2025-10-24 13:42:13 +0000

    (Math.random() * (max-min+1)) + min

---
+++ 01_basics/numsandmath.js
@@ -0,0 +1,34 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(1000);
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+
+const number = 123.89764
+console.log(number.toPrecision(3));
+
+const hundreds = 1000000000;
+
+console.log(hundreds.toLocaleString('en-IN'));
+
+console.log(Math);
+console.log(Math.abs(-3));
+console.log(Math.round(-3.4));
+console.log(Math.ceil(3.4));
+console.log(Math.floor(3.4));
+
+console.log(Math.floor(Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min+1)) + min);
+console.log(Math.floor(Math.random() * (max-min+1)) + min);
+console.log(Math.floor(Math.random() * (max-min+1)) + min);
+console.log(Math.floor(Math.random() * (max-min+1)) + min);
+
